a4a021bb585b34492a624a9fe46f113455493e51
piyushpaliwalcodes <piyushpaliwalofficial868@gmail.com>
2025-07-12 17:03:25 +0530

    refactored our process

--- components/Ourprocess.tsx
+++ components/Ourprocess.tsx
@@ -25,18 +25,17 @@ const Ourprocess = () => {
         },
     ]
     return (
-        <div className="flex  gap-[150px]  justify-center px-[60px] py-[60px] bg-[#F5F5F5]">
-            <div className="flex flex-col gap-[30px] ">
-                <div className="flex gap-[5px]"> <Image src="/images/setting.png" alt="setting" width={10} height={10} className="w-[20px] h-[20px] " /> <p className="font-manrope font-medium uppercase tracking-widest
+        <div className="flex flex-col   justify-center px-[60px] py-[60px] bg-[#F5F5F5] gap-[50px]">
+            <div className="flex flex-col gap-[20px]"><div className="flex gap-[5px]"> <Image src="/images/setting.png" alt="setting" width={10} height={10} className="w-[20px] h-[20px] " /> <p className="font-manrope font-medium uppercase tracking-widest
                 ">Our Processes</p></div>
-                <p className="text-[48px]/[60px]">Streamlined processes <span className="font-bold">for optimal efficiency</span></p>
-                <Image src="/images/machinewithmala.png" alt="process" width={1000} height={1000} className="w-full" />
-            </div>
-            <div className="flex flex-col justify-around text-[#81848A] font-manrope text-[16px]">
-                  <p className="pl-[100px]">Our process is designed to maximize efficiency and quality at every stage
+            <div className="flex gap-[100px] items-end"> <p className="text-[48px]/[60px] ">Streamlined processes <span className="font-bold">for optimal efficiency</span></p>
+            <p className="w-2/3">Our process is designed to maximize efficiency and quality at every stage
 of production. By integrating advanced technologies and best practices,
-we ensure seamless workflows.</p>
-                   <div className="flex flex-col gap-[20px]">
+we ensure seamless workflows.</p></div></div>
+            
+<div className="flex gap-[150px]">
+<Image src="/images/machinewithmala.png" alt="process" width={800} height={1000} className="w-[600px]" />
+<div className="flex flex-col gap-[20px]">
                     {
                         steps.map((step,index) => (
                             <div key={index} className={`flex gap-[20px] py-[25px] px-[25px] hover:cursor-pointer rounded-[8px] ${selectedStep === index ? "bg-[#FCD900] text-black" : ""}`} onClick={() => setSelectedStep(index)}>
@@ -50,7 +49,10 @@ we ensure seamless workflows.</p>
                     }
 
                    </div>
-            </div>
+</div>
+            
+           
+          
         </div>
     )
 }
