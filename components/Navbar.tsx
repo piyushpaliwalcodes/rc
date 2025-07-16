93ba34556adc115c1e889ba34e8150f854570b74
piyushpaliwalcodes <piyushpaliwalofficial868@gmail.com>
2025-07-16 14:56:53 +0530

    navbar corrected

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,34 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from './Logo'
 import YellowButton from './Yellowbutton'
 import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
 
 const Navbar = () => {
 
-    const [selected, setSelected] = useState('Home')
+    const router = useRouter()
+    const pathname = usePathname()
+    const [selected, setSelected] = useState(pathname.split('/')[1])
+
+
+    useEffect(() => {
+        setSelected(pathname.split('/')[1])
+    }, [pathname])
+
     const navItems = [
-        {name: 'Home', link: '/'},
-        {name: 'About Us', link: '/aboutus'},
-        {name: 'Products', link: '/products'},
-        {name: 'Machines', link: '/machines'},
-        {name: 'Sustainability', link: '/sustainability'},
+        {name: 'Home', link: '/', key: ''},
+        {name: 'About Us', link: '/aboutus', key: 'aboutus'},
+        {name: 'Products', link: '/products', key: 'products'},
+        {name: 'Machines', link: '/machines', key: 'machines'},
+        {name: 'Sustainability', link: '/sustainability', key: 'sustainability'},
     ]
   return (
     <div className='sticky top-0 z-50 py-[10px] w-full flex justify-between items-center bg-white text-black px-[60px] border-b border-[#CCCCCC]'>
         <Logo/>
         <ul className='flex gap-[40px]'>
             {navItems.map((item) => (
-                <li key={item.name} onClick={() => setSelected(item.name)} className={`cursor-pointer  hover:text-[#FCD900] transition-all duration-100 ${selected === item.name ? 'text-[#FCD900] font-bold' : 'text-black'}`}>
+                <li key={item.name} className={`cursor-pointer  hover:text-[#FCD900] transition-all duration-100 ${selected === item.key ? 'text-[#FCD900] font-bold' : 'text-black'}`}>
                     <Link href={item.link}>{item.name}</Link>
                 </li>
             ))}
