7b1a94b2fffe76318f404085f5ec8301ca84b453
piyushpaliwalcodes <piyushpaliwalofficial868@gmail.com>
2025-07-16 17:45:42 +0530

    corrected

--- app/sustainability/page.tsx
+++ app/sustainability/page.tsx
@@ -6,7 +6,7 @@ import Withtractor from "@/components/Withtractor";
 
 const Sustainability = () => {      
     return (
-        <div className="flex flex-col bg-white text-black">
+        <div className="flex flex-col bg-white text-white">
             <Herowithnav title="Smart and Innovative Manufacturing Process for the Sustainability and Green Future." description="With more than 200,000 Retail outlets across 65+ branches, Ramlal Champalal (RC) is one of the largest Distribution House & Logistics services providers for various industries. With more than 200,000 Retail outlets across 65+ branches, Ramlal Champalal (RC) is one of the largest Distribution House & Logistics services providers for various industries."/>
             <Ourprocess/>
             <Greenery/>
