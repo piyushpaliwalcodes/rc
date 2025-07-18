c3d51c8c05fd0b6f6526950abd6601370825ffa5
piyushpaliwalcodes <piyushpaliwalofficial868@gmail.com>
2025-07-18 15:25:36 +0530

    hero second done

--- components/Herosecond.tsx
+++ components/Herosecond.tsx
@@ -3,10 +3,10 @@ import { JSX } from "react";
 const Herosecond = ({title}:{title:string}) =>
 {
     return (
-        <div className="flex flex-col gap-[10px] justify-center items-start p-[60px] h-[529px] relative" style={{backgroundImage: "url('/images/productsbg.jpg')", backgroundSize: "cover", backgroundPosition: "center"}}>
-            <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black/80 to-black/20 flex flex-col gap-[10px] justify-center items-start p-[60px]">
-            <h1 className="text-[70px]/[80px] text-white">{title}</h1>
-            <p className="text-[20px] flex gap-[10px] text-white pl-2">Home <ul className="flex gap-[10px] list-disc list-inside text-[#FCD900]"><li className="">Our Products</li></ul></p>
+        <div className="flex flex-col gap-2 md:gap-[10px] justify-center items-start p-4 md:p-[60px] h-[220px] md:h-[400px] lg:h-[529px] relative" style={{backgroundImage: "url('/images/productsbg.jpg')", backgroundSize: "cover", backgroundPosition: "center"}}>
+            <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black/80 to-black/20 flex flex-col gap-2 md:gap-[10px] justify-center items-start p-4 md:p-[60px]">
+                <h1 className="text-[28px] md:text-[48px] lg:text-[70px]/[80px] text-white">{title}</h1>
+                <p className="text-[14px] md:text-[20px] flex gap-2 md:gap-[10px] text-white pl-1 md:pl-2">Home <ul className="flex gap-2 md:gap-[10px] list-disc list-inside text-[#FCD900]"><li className="">Our Products</li></ul></p>
             </div>
         </div>
     )
