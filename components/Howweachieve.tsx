58e2e59c7f740a39ddf37e98a610ce8820db285c
piyushpaliwalcodes <piyushpaliwalofficial868@gmail.com>
2025-09-01 13:41:17 +0530

    feat: update Howweachieve component with new images and content
    - Updated image dimensions for better visibility.
    - Replaced existing images with new ones for enhanced visual appeal.
    - Revised titles and descriptions to better reflect achievements.
    - Improved text layout for clarity and engagement.
    - Changed background color for better contrast and aesthetics.

--- components/Howweachieve.tsx
+++ components/Howweachieve.tsx
@@ -3,7 +3,7 @@
 const Achieveitem = ({imagesrc,title,description}:any) =>
 {
     return <div className="flex flex-col gap-[10px] md:gap-[15px] items-center min-w-[160px] md:min-w-[0]">
-          <img height={"60px"} width={"60px"} src={imagesrc} />
+          <img height={"70px"} width={"70px"} src={imagesrc} />
           <p className="text-[24px] md:text-[40px] font-bold">{title}</p>
          
           <p className="max-w-[90%] md:max-w-[60%] font-bold text-[#464646] text-center text-[14px] md:text-base">{description}</p>
@@ -14,40 +14,41 @@ const Howweachieve = () =>
 {
     const highlightsdata = [
         {
-            imagesrc:"/images/partner.png",
-            title:"40+ ",
-            description:"TRUSTED MEMBERS",
+            imagesrc:"/images/a1.png",
+            title:"40000+ ",
+            description:"SQUARE-FEET-AREA",
         }
         ,
         {
-            imagesrc:"/images/cash.png",
+            imagesrc:"/images/a2.png",
             title:"40+ ",
-            description:"TRUSTED MEMBERS",
+            description:"TRUSTED PARTNERS",
         },
         {
-            imagesrc:"/images/peoples.png",
-            title:"3800+ ",
-            description:"MT/MONTHS GOOD HANDLING",
+            imagesrc:"/images/a3.png",
+            title:"5-6K ",
+            description:"QUINTALS/MONTH GOODS HANDLING",
         },
         {
-            imagesrc:"/images/group.png",
-            title:"2800+ ",
+            imagesrc:"/images/a4.png",
+            title:"50+ ",
             description:"CRORE INR TURNOVER",
         },
         {
-            imagesrc:"/images/peoples.png",
-            title:"40+ ",
-            description:"TRUSTED MEMBERS",
+            imagesrc:"/images/a5.png",
+            title:"30+ ",
+            description:"TEAM MEMBERS",
         }
     ]
 return (
-    <div className='w-full max-w-full px-4 md:px-8 lg:px-[60px] py-6 md:py-[60px] bg-[#F9F9F9] text-black'>
+    <div className='w-full max-w-full px-4 md:px-8 lg:px-[60px] py-6 md:py-[60px] bg-[#F9F9F9] text-black bg-[#FFFCE8]'>
          <div className="flex flex-col gap-4 md:gap-[20px]">
                 <p className="text-[16px] md:text-[20px] font-bold opacity-[30%]">HOW WE ACHIEVE</p>
                 <div className="flex flex-col lg:flex-row gap-4 lg:gap-[150px]">
-                    <p className="text-[22px] md:text-[32px] lg:text-[40px]/[50px] min-w-0 lg:min-w-[600px]">The Relentless Efforts And <span className="yellounderline">Achievements</span> Of Our Farmers</p>
-                    <div className="text-[14px] md:text-[18px] text-[#464646]">
-                        For almost 5 decades, the RC has dominated the integrated supply chain industry. Headquartered in Khandwa, Madhya Pradesh, India, we redefine supply chain management with precision, efficiency, and innovation. Our relentless commitment to excellence promises solutions.
+                    <p className="text-[22px] md:text-[32px] lg:text-[40px]/[50px] min-w-0 lg:min-w-[500px]">  <span className="yellounderline">Driven by Systems,</span> <br/>
+                    Sustained by People.</p>
+                    <div className="text-[14px] md:text-[18px] text-[#464646] ">
+                    At RC Pulses, our growth is built on more than numbers — it’s built on clarity, consistency, and collective effort. Every part of our ecosystem works in sync: from sourcing the best-quality pulses to processing them through precise, tech-enabled workflows. Behind every metric lies a team, a method, and a commitment to improve each day. That’s how we achieve — together.
                     </div>
                 </div>
             <div className="flex overflow-x-auto md:overflow-x-visible justify-start md:justify-around pt-8 md:pt-[60px] gap-4 md:gap-0">
