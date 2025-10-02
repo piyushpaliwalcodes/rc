950e5d5206558fe2a09047dd983ad5d4e9aece55
piyushpaliwalcodes <piyushpaliwalofficial868@gmail.com>
2025-10-02 14:40:55 +0530

    home page navigation from logo added

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import Image from 'next/image'
+'use client'
 
-const Logo = ({className}:{className?:string}) => {
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+interface LogoProps {
+  className?: string
+}
+
+const Logo: React.FC<LogoProps> = ({ className }) => {
+  const router = useRouter()
+
   return (
-    <div>
-        <Image
-          src="/rclogo.png"
-          alt="logo"
-          width={100}
-          height={50}
-          className={`h-[50px] w-auto ${className}`}
-        />
+    <div
+      onClick={() => router.push('/')}
+      className='hover:cursor-pointer'
+    >
+      <Image
+        src="/rclogo.png"
+        alt="RC Pulses Logo"
+        width={100}
+        height={50}
+        className={`h-[50px] w-auto ${className}`}
+        priority={true} // optional: preloads logo for better performance
+      />
     </div>
   )
 }
